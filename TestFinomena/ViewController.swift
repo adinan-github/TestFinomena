184098cafa9bd6ee4ce56484436cf21f2d28d273
adinan yujaroen <adinan.yujaroen@scbtechx.io>
2022-02-01 21:08:58 +0700

    Initial Commit

---
+++ TestFinomena/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFinomena
+//
+//  Created by ADINAN YUJAROEN on 1/2/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
